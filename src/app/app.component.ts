6e7fefc42b27c0226e5ac9a856822454b2bcc1a2
VyacheslavKozloff <vyacheslavkozlof@gmail.com>
2025-02-10 21:58:10 +0300

    Компонент отображения диаграммы добавлен в главный компонент

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BarChartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
