2bd7eab4f2be76f926d7e4aea656144a48114950
VyacheslavKozloff <vyacheslavkozlof@gmail.com>
2025-02-10 21:50:47 +0300

    Добавлена имплементация onInit и логика для рисования диаграммы

--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-bar-chart',
@@ -7,6 +8,28 @@ import { Component } from '@angular/core';
   templateUrl: './bar-chart.component.html',
   styleUrl: './bar-chart.component.css'
 })
-export class BarChartComponent {
+export class BarChartComponent implements OnInit {
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.createBarChart();
+  }
+ 
+  private createBarChart(): void {
+    const data = [30, 200, 100, 400, 150, 250];
+    const svg = d3.select("app-bar-chart").append("svg")
+                  .attr("width", 700)
+                  .attr("height", 300);
+    
+    svg.selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("x", (d, i) => i * 70)
+      .attr("y", d => 300 - d)
+      .attr("width", 65)
+      .attr("height", d => d)
+      .attr("fill", "blue");
+  }
 }
