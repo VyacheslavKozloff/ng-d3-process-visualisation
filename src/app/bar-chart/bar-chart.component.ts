10c4d600ed4d6a895496cfee4eb20dc46730ce12
VyacheslavKozloff <vyacheslavkozlof@gmail.com>
2025-02-11 22:58:30 +0300

    Пример использования группы

--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -18,18 +18,36 @@ export class BarChartComponent implements OnInit {
  
   private createBarChart(): void {
     const data = [30, 200, 100, 400, 150, 250];
+    const data2 = ['Process 1', 'Process 2', 'Process 3', 'Process 4'];
     const svg = d3.select("app-bar-chart").append("svg")
                   .attr("width", 700)
                   .attr("height", 300);
-    
-    svg.selectAll("rect")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("x", (d, i) => i * 70)
-      .attr("y", d => 300 - d)
-      .attr("width", 65)
-      .attr("height", d => d)
-      .attr("fill", "blue");
+
+    const groups = svg.selectAll('g').data(data2).join('g');
+
+    groups.append('rect')
+      .attr("width", 80) 
+      .attr("height", 40)
+      .attr("x", (d, i) => (100 * i))
+      .attr("y", 0)
+      .attr('fill', 'red');
+
+    groups.append('text')
+      .text(d => d)
+      .attr("x", (d, i) => (100 * i) + 60)
+      .attr("y", 12)
+      .attr("text-anchor", "end")
+      .style("font-family", "sans-serif")
+      .style("font-size", "11px");;
+
+    // svg.selectAll("rect")
+    //   .data(data)
+    //   .enter()
+    //   .append("rect")
+    //   .attr("x", (d, i) => i * 70)
+    //   .attr("y", d => 300 - d)
+    //   .attr("width", 65)
+    //   .attr("height", d => d)
+    //   .attr("fill", "blue");
   }
 }
