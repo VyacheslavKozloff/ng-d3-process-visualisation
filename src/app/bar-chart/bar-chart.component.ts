be978c4b1c86e955c26c175f0bed17c9841363e0
VyacheslavKozloff <vyacheslavkozlof@gmail.com>
2025-02-22 22:06:28 +0300

    Работа с анимацией процессов

--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -52,5 +52,94 @@ export class BarChartComponent implements OnInit {
     //   .attr("width", 65)
     //   .attr("height", d => d)
     //   .attr("fill", "blue");
+
+
+    const process_1 = {
+      id: 'group_1',
+      label: 'ПИ',
+      docs: [
+          {label: 'Протокол ПИ', isAdded: true},
+          {label: 'Акт', isAdded: false}
+      ],
+      isActive: false
+  };
+
+  const process_2 = {
+      id: 'group_2',
+      label: 'ПСИ',
+      docs: [
+          {label: 'Протокол ПСИ', isAdded: false},
+      ],
+      isActive: false
+  };
+
+  const width = 700;
+  const height = 300;
+
+  const data = [process_1, process_2];
+  const svg = d3.select(".processes_container").append("svg")
+                .attr("width", width)
+                .attr("height", height);
+
+  const groups = svg.selectAll('g')
+      .data(data)
+      .join('g')
+      .attr('transform', d => translate(0, 20))
+      .attr('id', d => d.id);
+
+  groups.append('rect')
+      .attr("width", 200) 
+      .attr("height", 40)
+      .attr("x", (d, i) => (210 * i) + 20)
+      .attr("y", 0)
+      .attr('fill', 'white')
+      .attr('stroke', '#E5E5E5')
+      .attr('rx', 15)
+      .attr('ry', 15)
+      .attr('transform', translate(0, 20))
+      .on('mouseover', (e, d) => {
+          d.isActive = true;
+          d3.select(e.target).classed('rect__expanded', true);
+          const rect = d3.select('.rect__expanded');
+
+          d3.select(#${d.id}).append('g').data(d.docs).append('rect')
+              .attr("width", 100) 
+              .attr("height", 40)
+              .attr("x", (d, i) => (210 * i) + 20)
+              .attr("y", 150)
+              .attr('fill', 'red');
+          
+
+          
+              // .join('rect')
+              // .attr("width", 200) 
+              // .attr("height", 40)
+              // .attr("x", (d, i) => (210 * i) + 20)
+              // .attr("y", 20)
+              // .attr('fill', 'red');
+
+          console.log(d3.select(#${d.id}));
+          
+      })
+      .on('mouseout', (e, d) => {
+          d.isActive = false;
+          d3.select(e.target).classed('rect__expanded', false);
+      });
+
+  groups.append('text')
+    .text(d => d.label)
+    .attr("x", (d, i) => (220 * i) + 60)
+    .attr("y", 0)
+    .attr('transform', translate(0, 45))
+    .style("font-family", "sans-serif")
+    .style("font-size", "11px");
+
+
+  // groups.append('circle')
+  //     .attr('cx', (d, i) => (210 * i) + 40)
+  //     .attr('cy', 120)
+  //     .attr('r', 15)
+  //     .attr('fill', '#77aab8');
+
   }
 }
